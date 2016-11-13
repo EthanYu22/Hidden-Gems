d05ce73b13fddada4441783e5b97a3264a8e8e75
Ethan Yu <ethanyu22@gmail.com>
2016-11-12 16:15:51 -0800

    Users Update Info
    Added Update function

--- public/js/users.js
+++ public/js/users.js
@@ -24,8 +24,22 @@ user.prototype.editUser = function(id, firstname, lastname, email, gender, age, 
 	this.bio = bio;
 };
 
-user.prototype.updateUser = function(){
-	// body ...
+user.prototype.updateUser = function(fn, ln, email, gender, age, bio){
+	// Generate a new unique ID under posts/ database
+    // and create the node for this post
+    var editUser = database.ref('users/').push();
+	
+	// Now start saving the details of the node (user) into database
+	editUser.set({
+		userID:{
+			firstname: fn,
+			lastname: ln,
+			email: email,
+			gender: gender,
+			age: age,
+			bio: bio
+		}
+	});
 }
 
 user.prototype.updatePassword = function(password) {
