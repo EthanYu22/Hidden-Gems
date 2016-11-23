d94a5c00da56a93ab0210a56ab20b847908b5371
Dat Nguyen <datinguy@ucsc.edu>
2016-11-23 10:45:00 -0500

    Display comments on my_gems.html.

--- public/js/my_gems.js
+++ public/js/my_gems.js
@@ -1,4 +1,5 @@
 displayGems();
+displayFeedbacks();
 
 function displayGems() {
   // Grab ALL posts
@@ -15,9 +16,36 @@ function displayGems() {
 
         // Display locations
         var el = document.createElement('p');
-        el.innerHTML = "Title: <span style='color: blue'>" + data.title + "</span> posted on date: " + data.date;
+        el.innerHTML = "<b>Title:</b> <span style='color: blue'>" + data.title + "</span> <br />posted on date: " + data.date + "<br /><br />";
         my_gems.appendChild(el);
       }
+    })
+  });
+}
+
+function displayFeedbacks() {
+  // Grab ALL posts
+  var FeedbacksRef = firebase.database().ref('feedbacks/');
+
+  FeedbacksRef.once('value', function (feedbacks) {
+    feedbacks.forEach(function (fb) {
+      var fbRef = firebase.database().ref('feedbacks/' + fb.key);
+      // Grab the data
+      fbRef.once('value', function (snapshot) {
+        snapshot.forEach(function (childSnapshot) {
+          var data = childSnapshot.val();
+          // Display ONLY when maker_id matches curUserid;
+          if (data.userid == userid) {
+            // Add a wrapper
+            var my_gems = document.getElementById('feedbacks');
+
+            // Display locations
+            var el = document.createElement('p');
+            el.innerHTML = "<b>Feedback:</b> <span style='color: blue'>" + data.text + "</span> <br />posted on date: " + data.date + "<br /><br />";
+            my_gems.appendChild(el);
+          }
+        })
+      })
     })
   });
 }
